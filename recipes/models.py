f19983586ceb18d89b3aba3888aa268cb037729e
geohas <georg@hasibether.at>
2016-07-14 19:32:26 +0200

    init

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,10 @@
 # encoding utf-8
 
 from __future__ import unicode_literals
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.timezone import now
+
 
 # Create your models here.
 
@@ -20,6 +23,14 @@ class Category(models.Model):
 
 class Recipe(models.Model):
     """Recipe model"""
+    DIFFICULTY_EASY = 1
+    DIFFICULTY_MEDIUM = 2
+    DIFFICULTY_HARD = 3
+    DIFFICULTIES = (
+        (DIFFICULTY_EASY, u'einfach'),
+        (DIFFICULTY_MEDIUM, u'normal'),
+        (DIFFICULTY_HARD, u'schwer')
+    )
     title = models.CharField(u'Titel', max_length=255)
     slug = models.SlugField(unique=True)
     ingredients = models.TextField(u'Zutaten',
@@ -29,3 +40,23 @@ class Recipe(models.Model):
                                                help_text=u'Zeit in Minuten angeben',
                                                blank=True, null=True)
     number_of_portions = models.PositiveIntegerField(u'Anzahl der Portionen')
+    difficulty = models.SmallIntegerField(u'Schwierigkeitsgrad',
+                                          choices=DIFFICULTIES, default=DIFFICULTY_MEDIUM)
+    category = models.ManyToManyField(Category, verbose_name=u'Kategorien')
+    author = models.ForeignKey(User, verbose_name=u'Autor')
+    date_created = models.DateTimeField(editable=False)
+    date_updated = models.DateTimeField(editable=False)
+
+    class Meta:
+        verbose_name = u'Rezept'
+        verbose_name_plural = u'Rezepte'
+        ordering = ['-date_created']
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.date_created = now()
+        self.date_updated = now()
+        super(Recipe,self).save(*args, **kwargs)
